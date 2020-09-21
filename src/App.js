5f72664fa5b61ffbda62bb2376f335320108acb9
Zuzki <ldegrac071@hotmail.com>
2020-09-21 08:06:42 -0500

    Set everything to prevent errors
    - Placeholder loading component
    
    - Key added to fragment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
 import React, { useEffect, useState, Fragment } from 'react';
-import {
-  Container,
-  Header,
-  NetWorkContainer,
-  NetWorksContainer,
-  NetWorkText,
-  OverviewContainer,
-  OverviewInfo,
-  OverViewText,
-  HeaderText,
-  HeaderMode,
-  BrandText,
-  UserNetWork, NetworkHeader
-} from "./AppStyles";
+import { Container, Header, NetWorkContainer, NetWorksContainer, NetWorkText, OverviewContainer, OverviewInfo, OverViewText, HeaderText, HeaderMode, BrandText, UserNetWork, NetworkHeader } from "./AppStyles";
 import axios from './axios-orders'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab, faFacebook, faTwitter, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
@@ -21,63 +8,74 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
 
-  const [networks, setNetworks] = useState([])
+  const [ networks, setNetworks ] = useState([])
+  const [ loading, setLoading ] = useState(true)
+
   library.add( fab, faFacebook, faTwitter, faInstagram, faYoutube)
 
   useEffect(() => {
-    axios.get("/networks.json").then((response) => {
-      setNetworks(response.data);
-    });
-
-  },[])
+    if( loading ){
+      axios.get("/networks.json").then((response) => {
+        setNetworks(response.data);
+        setLoading( !loading );
+      });
+    }
+  },[loading])
   
-  return (
-    <Container>
-      <Header>
-        <HeaderText>Social Media Dashboard</HeaderText>
-        <HeaderText>Total Followers: ####</HeaderText>
-        <HeaderMode>Dark Mode</HeaderMode>
-      </Header>
-      <NetWorksContainer>
-        {networks.map((network) => {
-          const icon = network.name;
-
-          return (
-            <NetWorkContainer key={network.name}>
-              <NetworkHeader>
-                <BrandText brand={network.name} ><FontAwesomeIcon icon={["fab", icon]} /></BrandText>
-                <UserNetWork>@{network.user}</UserNetWork>
-              </NetworkHeader>
-              <NetWorkText>{network.followers}</NetWorkText>
-              <NetWorkText>FOLLOWERS</NetWorkText>
-            </NetWorkContainer>
-          );
-        })}
-      </NetWorksContainer>
-      <h2 style={{ margin: 0, color: "#fcdab7"}}>Overview - Today</h2>
-      <OverviewContainer>
-        {networks.map((network) => {
-          const icon = network.name;
-          return (
-            <Fragment>
-              <OverviewInfo>
-                <OverViewText>Views</OverViewText>
-                <BrandText brand={network.name} ><FontAwesomeIcon icon={["fab", icon]} /></BrandText>
-                <OverViewText>{network.views}</OverViewText>
-                <OverViewText>Some Text</OverViewText>
-              </OverviewInfo>
-              <OverviewInfo>
-                <OverViewText>Likes</OverViewText>
-                <BrandText id={network.name} brand={network.name}><FontAwesomeIcon icon={["fab", icon]} /></BrandText>
-                <OverViewText>{network.likes}</OverViewText>
-                <OverViewText>Some Text </OverViewText>
-              </OverviewInfo>
-            </Fragment>
-          );
-        })}
-      </OverviewContainer>
-    </Container>
-  );
+  if( loading ){
+    return (
+      <div>
+        Loading...
+      </div>
+    )
+  }else{
+    return (
+      <Container>
+        <Header>
+          <HeaderText>Social Media Dashboard</HeaderText>
+          <HeaderText>Total Followers: ####</HeaderText>
+          <HeaderMode>Dark Mode</HeaderMode>
+        </Header>
+        <NetWorksContainer>
+          {networks.map((network) => {
+            const icon = network.name;
+            return (
+              <NetWorkContainer key={network.name}>
+                <NetworkHeader>
+                  <BrandText brand={network.name} ><FontAwesomeIcon icon={["fab", icon]} /></BrandText>
+                  <UserNetWork>@{network.user}</UserNetWork>
+                </NetworkHeader>
+                <NetWorkText>{network.followers}</NetWorkText>
+                <NetWorkText>FOLLOWERS</NetWorkText>
+              </NetWorkContainer>
+            );
+          })}
+        </NetWorksContainer>
+        <h2 style={{ margin: 0, color: "#fcdab7"}}>Overview - Today</h2>
+        <OverviewContainer>
+          {networks.map((network) => {
+            const icon = network.name;
+            return (
+              <Fragment key={network.name + '-overview'}>
+                <OverviewInfo >
+                  <OverViewText>Views</OverViewText>
+                  <BrandText brand={network.name} ><FontAwesomeIcon icon={["fab", icon]} /></BrandText>
+                  <OverViewText>{network.views}</OverViewText>
+                  <OverViewText>Some Text</OverViewText>
+                </OverviewInfo>
+                <OverviewInfo>
+                  <OverViewText>Likes</OverViewText>
+                  <BrandText id={network.name} brand={network.name}><FontAwesomeIcon icon={["fab", icon]} /></BrandText>
+                  <OverViewText>{network.likes}</OverViewText>
+                  <OverViewText>Some Text </OverViewText>
+                </OverviewInfo>
+              </Fragment>
+            );
+          })}
+        </OverviewContainer>
+      </Container>
+    );
+  }
 }
 
 export default App;
