3b81da607b2657df35eb9ff3ccf9932c7052ac85
Zuzki <ldegrac071@hotmail.com>
2020-09-24 20:04:46 -0500

    Moved every component to their own file

--- src/App.js
+++ src/App.js
@@ -1,81 +1,46 @@
+import { fab, faFacebook, faTwitter, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import OverView from './Components/OverViewContainer/OverViewContainer';
+import Networks from './Components/NetworksComponent/NetworksComponent';
+import Header from './Components/HeaderComponent/HeaderComponent';
 import React, { useEffect, useState, Fragment } from 'react';
-import { Container, Header, NetWorkContainer, NetWorksContainer, NetWorkText, OverviewContainer, OverviewInfo, OverViewText, HeaderText, HeaderMode, BrandText, UserNetWork, NetworkHeader } from "./AppStyles";
-import axios from './axios-orders'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab, faFacebook, faTwitter, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { Container } from "./AppStyles";
+import axios from './axios-orders';
 
 
 function App() {
 
-  const [ networks, setNetworks ] = useState([])
-  const [ loading, setLoading ] = useState(true)
-
+  const [networks, setNetworks ] = useState([])
   library.add( fab, faFacebook, faTwitter, faInstagram, faYoutube)
 
   useEffect(() => {
-    if( loading ){
-      axios.get("/networks.json").then((response) => {
-        setNetworks(response.data);
-        setLoading( !loading );
-      });
-    }
-  },[loading])
-  
-  if( loading ){
-    return (
-      <div>
-        Loading...
-      </div>
-    )
-  }else{
-    return (
-      <Container>
-        <Header>
-          <HeaderText>Social Media Dashboard</HeaderText>
-          <HeaderText>Total Followers: ####</HeaderText>
-          <HeaderMode>Dark Mode</HeaderMode>
-        </Header>
-        <NetWorksContainer>
-          {networks.map((network) => {
-            const icon = network.name;
-            return (
-              <NetWorkContainer key={network.name}>
-                <NetworkHeader>
-                  <BrandText brand={network.name} ><FontAwesomeIcon icon={["fab", icon]} /></BrandText>
-                  <UserNetWork>@{network.user}</UserNetWork>
-                </NetworkHeader>
-                <NetWorkText>{network.followers}</NetWorkText>
-                <NetWorkText>FOLLOWERS</NetWorkText>
-              </NetWorkContainer>
-            );
-          })}
-        </NetWorksContainer>
-        <h2 style={{ margin: 0, color: "#fcdab7"}}>Overview - Today</h2>
-        <OverviewContainer>
-          {networks.map((network) => {
-            const icon = network.name;
-            return (
-              <Fragment key={network.name + '-overview'}>
-                <OverviewInfo >
-                  <OverViewText>Views</OverViewText>
-                  <BrandText brand={network.name} ><FontAwesomeIcon icon={["fab", icon]} /></BrandText>
-                  <OverViewText>{network.views}</OverViewText>
-                  <OverViewText>Some Text</OverViewText>
-                </OverviewInfo>
-                <OverviewInfo>
-                  <OverViewText>Likes</OverViewText>
-                  <BrandText id={network.name} brand={network.name}><FontAwesomeIcon icon={["fab", icon]} /></BrandText>
-                  <OverViewText>{network.likes}</OverViewText>
-                  <OverViewText>Some Text </OverViewText>
-                </OverviewInfo>
-              </Fragment>
-            );
-          })}
-        </OverviewContainer>
-      </Container>
-    );
+    axios.get("/networks.json").then((response) => {
+      setNetworks(response.data);
+    });
+  },[])
+
+  const totalFollowersHandler = () => {
+    let followers = 0
+
+    networks.map( network => {
+      return followers += network.followers;
+    })
+    return followers
   }
+
+  return (
+    <Fragment>
+      { networks.length === 0 ? (
+        <div>Loading...</div>
+      ) : (
+        <Container>
+          <Header followers={ totalFollowersHandler() } />
+          <Networks networks={ networks }/>
+          <OverView networks={ networks }/>
+        </Container>
+      )}
+    </Fragment>
+  );
 }
 
 export default App;
