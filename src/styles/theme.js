8f3af6c35f9549ff35235a19f7fa43b2df50dae2
Zuzki <ldegrac071@hotmail.com>
2020-09-26 17:18:18 -0500

    Global theme file

---
+++ src/styles/theme.js
@@ -0,0 +1,28 @@
+const theme = {
+  lightTheme: {
+    colors: {
+      background: "#ffffff",
+      black: "black",
+      white: "white",
+      dark: "#8F8B7E",
+      light: "#e1f4f3",
+      secondary: "radial-gradient(green, yellow)",
+      text: "#333333",
+    },
+    transition: "0.5s",
+  },
+  darkTheme: {
+    colors: {
+      background: "#1b1b2f",
+      black: "white",
+      white: "black",
+      dark: "#162447",
+      light: "#1f4068",
+      secondary: "#6550E6",
+      text: "#fff",
+    },
+    transition: "0.5s",
+  },
+};
+
+export default theme;
