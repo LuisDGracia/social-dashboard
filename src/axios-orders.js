aa75062cce5f39a1fa004135b83b7a90cec3e5cc
Luis De Gracia <ldegrac071@hotmail.com>
2020-09-26 18:54:54 -0500

    Update axios-orders.js

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import './config/config'
 
 const instance = axios.create({
-  baseURL: process.env.FB_URL,
+  baseURL: process.env.REACT_APP_FB_URL,
 });
 
 export default instance
