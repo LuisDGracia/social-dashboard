a96e1b57ed177dd1a721f7140e34592c4029d80b
Zuzki <ldegrac071@hotmail.com>
2020-09-26 18:01:04 -0500

    fixed an error with env variables

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_FB_URL
+  baseURL: process.env.FB_URL || process.env.REACT_APP_FB_URL,
 });
 
 export default instance
