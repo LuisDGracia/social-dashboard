eb9669feb47462c58ef6de4539e485674c0c5962
Zuzki <ldegrac071@hotmail.com>
2020-09-26 18:34:52 -0500

    New try to fix some error

---
+++ config/config.js
@@ -0,0 +1,4 @@
+// ============
+//    ENTORNO
+// ============
+process.env.FB_URL = process.env.FB_URL || process.env.REACT_APP_FB_URL;
--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import '../config/config'
 
 const instance = axios.create({
-  baseURL: process.env.FB_URL || process.env.REACT_APP_FB_URL,
+  baseURL: process.env.FB_URL,
 });
 
 export default instance
