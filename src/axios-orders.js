ada8a04a3216fbdf17421dd46b542e00d79e86f6
Zuzki <ldegrac071@hotmail.com>
2020-09-28 14:00:52 -0500

    something

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_FB_URL
+  // No deberia dejar la url aqui
+  baseURL: process.env.REACT_APP_FB_URL || "https://social-dashboard-b240c.firebaseio.com/"
 });
 
 export default instance
